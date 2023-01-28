09538e7ac344d1bf2aef1819f279fcebd976747c
Bimal Ray <72511459+zenn99-arch@users.noreply.github.com>
2023-01-28 15:55:45 +0530

    chanegd

---
+++ .next/static/webpack/pages/showcase5/showcase5-dark.0fcd543edf5c123c.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/showcase5/showcase5-dark",{
+
+/***/ "./src/data/showcases-website.json":
+/*!*****************************************!*\
+  !*** ./src/data/showcases-website.json ***!
+  \*****************************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('[{"id":1,"title":{"first":"","second":"Visit"},"image":"/img/portfolio/works/1.png","sub":"Design","href":"https://proflou.com/"},{"id":2,"title":{"first":"Cosmetic","second":"Ad"},"image":"/img/portfolio/works/2.png","sub":"Design"},{"id":3,"title":{"first":"Device","second":"Model"},"image":"/img/portfolio/works/3.png","sub":"Design"},{"id":4,"title":{"first":"Abstract","second":"Art"},"image":"/img/portfolio/works/6.png","sub":"Design"},{"id":5,"title":{"first":"Printer","second":"Ad"},"image":"/img/portfolio/works/4.png","sub":"Design"},{"id":6,"title":{"first":"Bakery","second":"Ad"},"image":"/img/portfolio/works/5.png","sub":"Design"},{"id":7,"title":{"first":"Allure","second":"Ad"},"image":"/img/portfolio/works/7.png","sub":"Design"},{"id":8,"title":{"first":"Bugati","second":"Model"},"image":"/img/portfolio/works/8.png","sub":"Design"},{"id":9,"title":{"first":"Logo","second":"Model"},"image":"/img/portfolio/works/17.png","sub":"Design"},{"id":10,"title":{"first":"Isometric","second":"Model"},"image":"/img/portfolio/works/21.png","sub":"Design"}]');
+
+/***/ })
+
+});
